5f5a55814d45bfb1af9405997ec4fa53d5f3d9a4
melissasjostedt <97900446+melissasjostedt@users.noreply.github.com>
2022-02-25 17:49:50 +0100

    Add files via upload

---
+++ database.js
@@ -0,0 +1,60 @@
+
+let database= [
+    {
+    id: 1,
+    actor: "Nina Dobrev",
+    age: 33,
+    role: "Elena Gilbert",
+    breed: "Human",
+},
+
+{
+    id: 2,
+    actor: "Paul Wesley",
+    age: 39,
+    role: "Stefan Salvatore",
+    breed: "Vampire",
+},
+
+{
+   id: 3,
+   actor: "Ian Somerhalder",
+   age: 43,
+   role: "Elena Gilbert",
+   breed: "Vampire",
+},
+
+{
+id: 4,
+actor: "Katerina Graham",
+age: 32,
+role: "Bonnie Bennett",
+breed: "Witch",
+},
+
+{
+id: 5,
+actor: "Candice King",
+age: 34,
+role: "Caroline Forbes",
+breed: "Human",
+},
+
+{
+id: 6,
+actor: "Michael Trevino",
+age: 37,
+role: "Taylor Lockwood",
+breed: "werewolf",
+},
+
+{
+    id: 7,
+    actor: "Zack Roerig",
+    age: 37,
+    role: "Matt Donovan",
+    breed: "Human",
+}
+
+
+]
---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict"
+
+
+function createNewCharacter(actor, age, role, breed) {
+    let artist = {
+        actor: actor,
+        age: age,
+        role: role,
+        breed: breed,
+
+    };
+}
+return character; 
